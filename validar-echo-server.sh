dd2cfdf6c31ec85b584b53dd4d1e2685450df1da
ijolodovsky <ijolodovsky@fi.uba.ar>
2025-08-24 20:02:33 -0300

    fix: correct command syntax for echo in validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RESULT=$(echo "hello" | docker run --rm --network tp0_testing_net busybox nc server 12345)
+RESULT=$(docker run --rm --network tp0_testing_net busybox -c "echo hello | nc server 12345")
 
 if [ "$RESULT" = "hello" ]; then
   echo "action: test_echo_server | result: success"
