3340947d366740c331ff605deff4b76e7964c37e
ijolodovsky <ijolodovsky@fi.uba.ar>
2025-08-24 19:52:48 -0300

    fix: use single equals for string comparison in validar-echo-server.sh

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -2,7 +2,7 @@
 
 RESULT=$(echo "hello" | docker run --rm --network tp0_testing_net busybox nc server 12345)
 
-if [ "$RESULT" == "hello" ]; then
+if [ "$RESULT" = "hello" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
