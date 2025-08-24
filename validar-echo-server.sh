50fbb606b6ed2ac9472459668604e161d9bb0964
ijolodovsky <ijolodovsky@fi.uba.ar>
2025-08-24 19:44:21 -0300

    fix: update network name in validar-echo-server.sh to match testing configuration

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-RESULT=$(echo "hello" | docker run --rm --network testing_net busybox nc server 12345)
+RESULT=$(echo "hello" | docker run --rm --network tp0_testing_net busybox nc server 12345)
 
 if [ "$RESULT" == "hello" ]; then
   echo "action: test_echo_server | result: success"
 else
   echo "action: test_echo_server | result: fail"
 fi
-
-docker stop server
-docker rm server
-docker network rm testing_net
